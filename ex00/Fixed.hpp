bdb2378b18ed0274edd6098c4bf31bbeea63ac52
Hamza Boussetta <hbousset@c1r2p14.1337.ma>
2025-09-29 15:10:31 +0100

    newfiles

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:58:11 by hbousset          #+#    #+#             */
-/*   Updated: 2025/09/25 14:58:27 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/09/29 15:08:22 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,21 @@
 
 #include <iostream>
 
+class Fixed
+{
+	private:
+		int					_value;
+		static const int	_fractionalBits = 8;
+	public:
+		//constructor
+		Fixed();
+		//copy constrcutor
+		Fixed(const Fixed& other);
+		//copy assignment operator overload
+		Fixed&	operator=(const Fixed& other);
+		//destructor
+		~Fixed();
+		//member functions
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,13 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/25 14:57:59 by hbousset          #+#    #+#             */
-/*   Updated: 2025/09/25 14:58:08 by hbousset         ###   ########.fr       */
+/*   Created: 2025/09/28 18:09:49 by hbousset          #+#    #+#             */
+/*   Updated: 2025/09/29 14:38:00 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
