339e4784d20802c3108d8d3928b2e913269ab724
Hamza Boussetta <hbousset@c1r1p12.1337.ma>
2025-10-12 09:53:20 +0100

    Implement bsp function to check point inside a triangle

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -30,3 +30,5 @@ class Point
 		Fixed getX() const;
 		Fixed getY() const;
 };
+
+bool bsp( Point const a, Point const b, Point const c, Point const point);
--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,19 +6,20 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 12:29:39 by hbousset          #+#    #+#             */
-/*   Updated: 2025/10/11 12:54:39 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/10/12 09:36:57 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-// • a, b, c: The vertices of our beloved triangle.
-// • point: The point to check.
-// • Returns: True if the point is inside the triangle. False otherwise.
-// Thus, if the point is a vertex or on an edge, it will return False.
-// Implement and turn in your own tests to ensure that your class behaves as expected.
-
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-
+	Fixed	area1 = ((b.getX() - a.getX()) * (point.getY() - a.getY())) - ((b.getY() - a.getY()) * (point.getX() - a.getX()));
+	Fixed	area2 = ((c.getX() - b.getX()) * (point.getY() - b.getY())) - ((c.getY() - b.getY()) * (point.getX() - b.getX()));
+	Fixed	area3 = ((a.getX() - c.getX()) * (point.getY() - c.getY())) - ((a.getY() - c.getY()) * (point.getX() - c.getX()));
+	if (area1 == 0 || area2 == 0 || area3 == 0)
+		return false;
+	if ((area1 > 0 && area2 > 0 && area3 > 0) || (area1 < 0 && area2 < 0 && area3 < 0))
+		return true;
+	return false;
 }
