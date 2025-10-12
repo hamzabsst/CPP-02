d99b4906daaf4edc03e369c99182668ee0afa472
Hamza Boussetta <hbousset@c1r1p12.1337.ma>
2025-10-12 10:53:03 +0100

    Refactor: Improve bsp function clarity and remove zero area edge case

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,20 +6,24 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 12:29:39 by hbousset          #+#    #+#             */
-/*   Updated: 2025/10/12 09:36:57 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/10/12 10:51:14 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool bsp( Point const a, Point const b, Point const c, Point const point)
+static Fixed crossProduct(Point const& p1, Point const& p2, Point const& p3)
 {
-	Fixed	area1 = ((b.getX() - a.getX()) * (point.getY() - a.getY())) - ((b.getY() - a.getY()) * (point.getX() - a.getX()));
-	Fixed	area2 = ((c.getX() - b.getX()) * (point.getY() - b.getY())) - ((c.getY() - b.getY()) * (point.getX() - b.getX()));
-	Fixed	area3 = ((a.getX() - c.getX()) * (point.getY() - c.getY())) - ((a.getY() - c.getY()) * (point.getX() - c.getX()));
-	if (area1 == 0 || area2 == 0 || area3 == 0)
+	return ((p2.getX() - p1.getX()) * (p3.getY() - p1.getY())) - ((p2.getY() - p1.getY()) * (p3.getX() - p1.getX()));
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point)
+{
+	Fixed d1 = crossProduct(a, b, point);
+	Fixed d2 = crossProduct(b, c, point);
+	Fixed d3 = crossProduct(c, a, point);
+
+	if (d1 == 0 || d2 == 0 || d3 == 0)
 		return false;
-	if ((area1 > 0 && area2 > 0 && area3 > 0) || (area1 < 0 && area2 < 0 && area3 < 0))
-		return true;
-	return false;
+	return (d1 > 0 && d2 > 0 && d3 > 0) || (d1 < 0 && d2 < 0 && d3 < 0);
 }
